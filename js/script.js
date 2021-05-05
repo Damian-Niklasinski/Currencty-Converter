a0e37e4d6e431ef44cac5885034ab38ebfa63a76
Damian Niklasiński <damianiklasinski@wp.pl>
2021-05-05 17:46:36 +0200

    added calcuteResult, and init functions

--- js/script.js
+++ js/script.js
@@ -1,17 +1,33 @@
-let plnElement = document.querySelector(".js-plnValue");
-let eurElement = document.querySelector(".js-eurValue");
-let usdElement = document.querySelector(".js-usdValue");
-let gbpElement = document.querySelector(".js-gbpValue");
-let formElement = document.querySelector(".js-form");
+{
 
-formElement.addEventListener("submit", (event) => {
-  event.preventDefault();
-  let pln = plnElement.value;
-  let eurRate = pln / 4.57;
-  let usdRate = pln / 3.82;
-  let gbpRate = pln / 5.26;
-  eurElement.innerText = eurRate.toFixed(2) + " €";
-  usdElement.innerText = usdRate.toFixed(2) + " $";
-  gbpElement.innerText = gbpRate.toFixed(2) + " £";
 
-});
+  const calculateResult = () => {
+
+    const plnElement = document.querySelector(".js-plnValue");
+    const eurElement = document.querySelector(".js-eurValue");
+    const usdElement = document.querySelector(".js-usdValue");
+    const gbpElement = document.querySelector(".js-gbpValue");
+
+    const pln = plnElement.value;
+    const eurRate = pln / 4.57;
+    const usdRate = pln / 3.82;
+    const gbpRate = pln / 5.26;
+    eurElement.innerText = eurRate.toFixed(2) + " €";
+    usdElement.innerText = usdRate.toFixed(2) + " $";
+    gbpElement.innerText = gbpRate.toFixed(2) + " £";
+
+  };
+
+  const init = () => {
+    const formElement = document.querySelector(".js-form");
+    formElement.addEventListener("submit", (event) => {
+      event.preventDefault();
+
+      calculateResult();
+
+    });
+  };
+
+  init();
+
+};
